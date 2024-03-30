2218bc2279a3554b60e93b6aaa0c0aa2ff839cb1
조현일 <himzei@gmail.com>
2024-03-30 12:17:49 +0900

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import NotFound from "./routes/NotFound";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import SignUp from "./routes/SignUp";
+import Profile from "./routes/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+import SocialKakao from "./routes/SocialKakao";
+import HomeToRoute from "./components/HomeToRoute";
+import TweetForm from "./routes/TweetForm";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "",
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "login",
+        element: (
+          <HomeToRoute>
+            <Login />
+          </HomeToRoute>
+        ),
+      },
+      {
+        path: "signup",
+        element: (
+          <HomeToRoute>
+            <SignUp />
+          </HomeToRoute>
+        ),
+      },
+      {
+        path: "tweet-write",
+        element: <TweetForm />,
+      },
+      {
+        path: "users/socials/kakao",
+        element: <SocialKakao />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
